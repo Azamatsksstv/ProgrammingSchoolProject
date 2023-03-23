f0c560ecdd88f1b4025e5e78e795ac4c1cc5575e
Azamat <azambylbaev@gmail.com>
2023-03-23 18:12:48 +0500

    token authentication with djoser

--- src/urls.py
+++ src/urls.py
@@ -4,9 +4,8 @@ from django.urls import path, include, re_path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.authtoken')),
-    path('auth/', include('djoser.urls.jwt')),
-    path('drf-auth/', include('rest_framework.urls')),
+    # path('auth/', include('djoser.urls.jwt')),
     path('api/v1/', include('lessons.urls')),
+    path('api/v1/auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
 ]
